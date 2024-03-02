56bce2feab96676e03da87d14d4b8618693aa2c1
Семенов <@RIB.local>
2024-03-02 14:14:10 +0300

    format code: deleting comments

--- src/shared/api/goods-slice.ts
+++ src/shared/api/goods-slice.ts
@@ -1,15 +1,35 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { BASE_URLS } from "./baseUrls";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URLS } from "./baseUrls";
 
+export const goodsSlice = createApi({
+  reducerPath: "goodsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URLS.VALANTIS_URL,
+    prepareHeaders: (headers) => {
+      headers.set("Authorization", "Barer ");
+      return headers;
+    },
+  }),
+  endpoints: (build) => ({
+    getGoods: build.query({
+      query: () => "/psdfsdf",
+    }),
+  }),
+});
 // const goodsSlice = createApi({
 //   reducerPath: "goodsApi",
 //   baseQuery: fetchBaseQuery({
 //     baseUrl: BASE_URLS.VALANTIS_URL,
 //     prepareHeaders: (headers) => {
-//       headers.set("authorization", "BEarer ");
+//       headers.set("Authorization", "Barer ");
 //       return headers;
 //     },
 //     tag.types:['goods'],
+//     ednpoints: (build)=>({
+//       getGoods: build.query({
+//         query:()=>'/payments',
+//       })
+//     })
 //   }),
 // });
 
